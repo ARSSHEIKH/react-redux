7a30451d2973e95dee9ae0d07978ab14aac98290
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2021-01-30 20:55:41 +0500

    Add files via upload

---
+++ src/store/reducer/index.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE={
+    students:[
+    {
+        name: 'ABC',
+        email: 'abc@gmail.com'
+    },
+    {
+        name: 'XYZ',
+        email: 'xyz@gmail.com'
+    }
+]
+}
+export default (state = INITIAL_STATE, action) => {
+    return state;
+}
